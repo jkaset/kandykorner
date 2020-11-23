4cfcd71e8aa7507bd77577968dec7934fdb3bd60
Jasmin Kaset <kasetski@gmail.com>
2020-11-23 16:13:20 -0600

    nav bar functional

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
+import { BrowserRouter as Router } from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import { KandyKorner } from '../src/components/KandyKorner'
 
 ReactDOM.render(
   <React.StrictMode>
-    <KandyKorner />
+    <Router>
+      <KandyKorner />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
