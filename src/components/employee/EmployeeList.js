96e1402a63a6c12fbe84abd55b7abb0499ecdd41
Jasmin Kaset <kasetski@gmail.com>
2020-11-25 09:37:36 -0600

    location name renders in Employees Tab

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,27 +1,38 @@
 import React, { useContext, useEffect } from "react"
 import { EmployeeContext } from "./EmployeeProvider"
+import { LocationContext } from "../location/LocationProvider"
 import { Employee } from "./Employee"
 import "./Employee.css"
 
 export const EmployeeList = (props) => {
   const { employees, getEmployees } = useContext(EmployeeContext)
+  const { locations, getLocations } = useContext(LocationContext)
 
 
   useEffect(() => {
     console.log("EmployeeList: Initial render before data")
-    getEmployees()
+    getLocations()
+      .then(getEmployees)
   }, [])
 
   return (
     <div className="employees">
-        <h1>Employees</h1>
-        <button onClick={() => props.history.push("/employees/create")}>
-            Add Employee
+      <h1>Employees</h1>
+      <button onClick={() => props.history.push("/employees/create")}>
+        Add Employee
         </button>
-        <article className="employeeList">
-            {employees.map(employee => <Employee key={employee.id} employee={employee} />)}
-        </article>
-    </div>
-)
+      <article className="employeeList">
+        {
+          employees.map(employee => {
+            const branch = locations.find(l => l.id === employee.locationId)
 
+            return <Employee key={employee.id}
+              location={branch}
+              employee={employee} />
+          })
+        }
+      </article>
+    </div>
+  )
 }
+
