b5c13c7635b7c36e3c472ce7b5df34b8f252de96
Jasmin Kaset <kasetski@gmail.com>
2020-11-30 15:02:37 -0600

    saves orders but not rendering

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,21 +1,37 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./Product.css"
+import { OrderContext } from "../customerCandy/OrderProvider"
 //import { Link } from "react-router-dom"
 //import {ProductType} from "../productType/ProductTypeProvider"
 //const mappedType = productType(pt => <li>{pt.name}</li>)
+export const Product = ({ props, product, productType }) => {
+  const { addOrder } = useContext(OrderContext)
+  const createOrder = (productId) => {
+    const customer = parseInt(localStorage.getItem('kandy_customer'))
 
-export const Product = ({ props, product, productType}) => (
-  <section className="product">
-    <h3 className="product__name">{product.name}</h3>
-    <p className="product__price"> Price: ${product.price}</p>
-    <p className="product__productTypeId">Type: {productType.name}
-    </p>
-    <button onClick={() => props.history.push("/orders/create")}>
-  Purchase
+    const newOrder = { productId, customer }
+
+    addOrder(newOrder)
+    //get customerId from local storage 
+    //and get productId with product.id, post to database
+  }
+  return (
+
+    <section className="product">
+      <h3 className="product__name">{product.name}</h3>
+      <p className="product__price"> Price: ${product.price}</p>
+      <p className="product__productTypeId">Type: {productType.name}
+      </p>
+      <button onClick={() => createOrder(product.id)
+      }>
+        Purchase
   </button>
-  </section>
-)
-{/* <button type="submit">Purchase</button> */}
+    </section>
+  )
+}
+
+
+{/* <button type="submit">Purchase</button> */ }
 
 // type="submit"
 //         onClick={evt => {
