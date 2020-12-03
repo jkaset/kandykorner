0267565cf5d2270c98d1b11e8f599a403b12f623
Jasmin Kaset <kasetski@gmail.com>
2020-12-02 20:34:53 -0600

    added button to Employee.js

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,13 +1,23 @@
 import React from "react"
 import "./Employee.css"
+import {EmployeeContext} from "./EmployeeProvider"
 
-export const Employee = ({ employee, location }) => (
+export const Employee = ({ employee, location, props}) => (
+    //const { removeEmployee } = useContext(EmployeeContext)
     <section className="employee">
         <h3 className="employee__name">{employee.name}</h3>
         <div className="employee__branch">Branch: {location.name}</div>
         <div className="employee__hourlyRate">Starting Hourly Rate: {employee.hourlyRate}</div>
         <div className="employee__fullTime">Full-time? {employee.fullTime}</div>
         <div className="employee__manager">Manager? {employee.manager}</div>
+        <button className="btn--release"
+                onClick={() => {
+                    removeEmployee(employee.id)
+                        .then(() => {
+                            props.history.push("/employees")
+                        })
+                }}
+            >Remove Employee</button>
     </section>
 )
 
